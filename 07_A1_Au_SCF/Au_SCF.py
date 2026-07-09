b0933158338c5abcaecbb3dacf671a1067feaa1a
Chris Woodgate <christopher.woodgate@gmail.com>
2026-07-09 12:16:31 +0100

    Addition of Au example

---
+++ 07_A1_Au_SCF/Au_SCF.py
@@ -0,0 +1,67 @@
+# SPR-KKR calculations on fcc Cu
+#
+# C. D. Woodgate, 2026
+
+def main():
+
+    # Import the relevant modules
+    import os
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from ase import Atoms
+    from ase.build import bulk
+
+    # I was using a local version of ase2sprkkr
+    # You should not need the below two lines if you have pip installed the package
+    #import sys
+    #sys.path.append('/path/to/ase2sprkkr/src')
+
+    from ase2sprkkr.sprkkr.calculator import SPRKKR
+    from ase2sprkkr.sprkkr.sprkkr_atoms import SPRKKRAtoms
+    from ase.eos import EquationOfState
+    from ase.units import Bohr,Rydberg,kJ,kB,fs,Hartree,mol,kcal
+
+    #=================================================#
+    # Step 1: Self-consistent field (SCF) calculation #
+    #=================================================#
+
+    # lattice parameter
+    a_lat = 4.17
+
+    # Make the directory we need (check if it exists first).
+    # (This means the SCF doesn't clutter the main directory)
+    directory = f'./scf'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Call 'bulk' to build me a bulk fcc crystal
+    atom = bulk('Au', 'fcc', a=a_lat)
+
+    # Now call ase2sprkkr to let me work on the atoms object
+    # in SPR-KKR mode. This allows specification of things like
+    # partial lattice site occupancies (for the CPA).
+    # (Don't actually need to do it here, but included for completeness.)
+    atoms=SPRKKRAtoms.promote_ase_atoms(atom)
+
+    # Now we set up the ASE calculator
+    # Specify how you run SPR-KKR here, along with controlling your PATH, etc.
+    calculator = SPRKKR(atoms=atoms,mpi=['mpirun','-np','96'], directory=directory)
+
+    # And the necessary parameters
+    calculator.input_parameters.set(NKTAB=250) # Control k-point sampling. 250 is default.
+    calculator.input_parameters.set(VXC='VWN') # VWN (LDA) XC functional
+    calculator.input_parameters.set(MODE='SREL') # Scalar relativistic mode
+    calculator.input_parameters.set(NL=3) # l_max=2 means 3 l-channels
+    calculator.input_parameters.set(NE=32) # 32-point semicircular energy contour
+    calculator.input_parameters.ENERGY.GRID=5 # Semicircular energy contour
+    calculator.input_parameters.SCF.MIX=0.05 # Gentle mixing paramter to ensure convergence
+    calculator.input_parameters.SCF.TOL=1e-6 # Tight tolerance on SCF cycle
+    calculator.input_parameters.SCF.NITER=200 # Allow for lots of iterations if needed
+    calculator.input_parameters.ENERGY.ImE=0.005 # Closest approach to real axis
+    
+    # Run the calculator
+    out=calculator.calculate()
+
+# If we run this script, execute 'main'
+if __name__ == "__main__":
+    main()
